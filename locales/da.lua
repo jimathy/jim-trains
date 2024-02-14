804fef4ed2cc9a30b71d46ef7c730ec7137fe36e
ARottenberger <69944020+iplayer1337fivem@users.noreply.github.com>
2024-02-14 23:46:11 +0100

    Create da.lua
    danish translation

---
+++ locales/da.lua
@@ -0,0 +1,16 @@
+Loc = Loc or {}
+Loc['da'] = {
+	metrain = 		"Metrotog",
+	freight = 		"Godstog",
+	buy = 			"Køb togbillet",
+	gettingoff = 	"Stå af på næste station",
+	getoff = 		"stig ud næste gang",
+	lsmetro = 		"LS Metro",
+	seating = 		"Find et sæde",
+	welcome = 		"Velkommen til LS Metro",
+	noticket = 		"Ingen billet, du eskorteres fra toget",
+	arrived = 		"Du er ankommet!",
+	already = 		"Fjollegøj, du har allerede en billet!",
+	purchased = 	"Køb billet",
+	nocash =		"Ikke nok kontanter",
+}
